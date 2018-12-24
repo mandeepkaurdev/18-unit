e4bf7bd3de75e86ef3c63c9c1366102171dd0921
Mandeep Kaur <mandeepkaur@mandeeps-mbp.attlocal.net>
2018-12-24 18:39:29 -0500

    Updates

---
+++ precache-manifest.9c78d7254045ebe63bc6239cefec8533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "562269d9066316e09f16",
+    "url": "/18-unit/static/js/runtime~main.562269d9.js"
+  },
+  {
+    "revision": "77fd2d3212cc3f66769a",
+    "url": "/18-unit/static/js/main.77fd2d32.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/18-unit/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "77fd2d3212cc3f66769a",
+    "url": "/18-unit/static/css/main.46c04fab.chunk.css"
+  },
+  {
+    "revision": "1c05347ce408a3aa42a648e4f24282c3",
+    "url": "/18-unit/index.html"
+  }
+];
